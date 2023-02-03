7f5e01c82b90245951572ca4b9f2305ddf9a9725
saimohanreddy98 <saimohanreddy98@gmail.com>
2023-02-03 10:53:07 +0530

    sssss

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -12,15 +12,14 @@ const Home = () => {
           <div class="image-list">
             <img src="https://n1.sdlcdn.com/imgs/k/f/v/12_april_WB_Kitchen_Essentials_WEB-cca82.jpg"/>
             </div>
-            <div class="image-list">
-              <img src="https://n1.sdlcdn.com/imgs/k/f/v/12_april_WB_Festive_kurta_sets_WEB_1-9d9b7.jpg"/>
-              </div>
-           
                 <div class="image-list">
                   <img src="https://n3.sdlcdn.com/imgs/j/8/e/Health_ID_Snapdeal_ABDM_1300X410-6c79d.jpg"/>
                   </div>
                   </div>
        <CatelogCard text="electronics" category="electronics" path="electronics"/>
+       <div class="image-list">
+              <img src="https://n1.sdlcdn.com/imgs/k/f/v/12_april_WB_Festive_kurta_sets_WEB_1-9d9b7.jpg"/>
+              </div>
       <CatelogCard text="cloth" category="cloth" path="cloth"/>
       <CatelogCard text="jewelery" category="jewelery" path="jewelery"/>
       
--- src/components/readme.md
+++ src/components/readme.md
@@ -8,7 +8,7 @@ let ans = await data.json();
   });
   console.log(x, "......  ..");
 }
-<div class="image-list">
+{/*<div class="image-list">
             <img src="https://n1.sdlcdn.com/imgs/k/f/v/12_april_WB_Festive_kurta_sets_WEB_1-9d9b7.jpg"/>
             </div>
             <div class="image-list">
@@ -16,4 +16,4 @@ let ans = await data.json();
               </div>
               <div class="image-list">
                 <img src="https://n3.sdlcdn.com/imgs/j/8/e/Health_ID_Snapdeal_ABDM_1300X410-6c79d.jpg"/>
-                </div>
+                </div>*/}
